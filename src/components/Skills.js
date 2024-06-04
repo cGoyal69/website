f5755355de13dee1a3d6e4ae6f5c865a248171a5
cGoyal69 <127645215+cGoyal69@users.noreply.github.com>
2024-06-05 00:25:05 +0530

    chart

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,9 +4,114 @@ import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
-import DoughnutChart from "./chart";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+Chart.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 export const Skills = () => {
+  const data1 = {
+    datasets: [
+      {
+        label: 'Web Developemnt',
+        data: [90,10],
+        backgroundColor: [
+          '#9900FF',
+          '#000000',
+          
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#000000',
+        ],
+      },
+    ],
+  };
+  const data2 = {
+    datasets: [
+      {
+        label: 'MERN Stack',
+        data: [95,5],
+        backgroundColor: [
+          '#9900FF',
+          '#000000',
+          
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#000000',
+        ],
+      },
+    ],
+  };
+  const data3 = {
+    datasets: [
+      {
+        label: 'Server Scripting',
+        data: [70,30],
+        backgroundColor: [
+          '#9900FF',
+          '#000000',
+          
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#000000',
+        ],
+      },
+    ],
+  };
+  const data4 = {
+    datasets: [
+      {
+        label: 'Competitive Programming',
+        data: [80,20],
+        backgroundColor: [
+          '#9900FF',
+          '#000000',
+          
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#000000',
+        ],
+      },
+    ],
+  };
+  const data5 = {
+    datasets: [
+      {
+        label: 'Documentation',
+        data: [70,30],
+        backgroundColor: [
+          '#9900FF',
+          '#000000',
+          
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#000000',
+        ],
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+      datalabels: {
+        color: 'white',
+        display: true,
+        formatter: (value) => `${value}%`,
+        font: {
+          weight: 'bold',
+          size: 14,
+        },
+      },
+    },
+  };
+
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -37,20 +142,36 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <DoughnutChart/>
+                              <div>
+							  <Doughnut data={data1} options={options} />
+                              </div>
+							  <h6 className='chartlabel'>Web Development</h6>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+							<div>
+							  <Doughnut data={data2} options={options} />
+                              </div>
+							  <h6 className='chartlabel'>MERN Stack</h6>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+							<div>
+							  <Doughnut data={data3} options={options} />
+                              </div>
+							  <h6 className='chartlabel'>Server Scripting</h6>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <div>
+							  <Doughnut data={data4} options={options} />
+                              </div>
+							  <h6 className='chartlabel'>Competitive Coding</h6>
                             </div>
+							<div className="item">
+                                <div>
+							  <Doughnut data={data4} options={options} />
+                              </div>
+							  <h6 className='chartlabel'>Documentation</h6>
+                            </div>
+
                         </Carousel>
                     </div>
                 </div>
