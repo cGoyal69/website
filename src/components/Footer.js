477ab74c38f5ccc494f19f6b7b6cd9b801399f2b
cGoyal69 <127645215+cGoyal69@users.noreply.github.com>
2024-06-05 00:30:59 +0530

    date function in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,18 @@ import logo from "../assets/img/img3.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import React from "react";
+import { useState } from "react";
+
+function getDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  return `${year}`;
+}
 
 export const Footer = () => {
+  
+  const [currentDate, setCurrentDate] = useState(getDate());
   return (
     <footer className="footer">
       <Container>
@@ -18,7 +28,7 @@ export const Footer = () => {
                 <a href="https://github.com/cGoyal69" target="blank"><img src={navIcon2} alt="" /></a>
                 <a href="https://www.instagram.com/_chirag2004/" target="blank"><img src={navIcon3} alt="" /></a>
             </div>
-            <p>Copyright 2024. All Rights Reserved</p>
+            <p>Copyright {currentDate}. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
