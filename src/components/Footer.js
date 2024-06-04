228364177cfde87fc6fa22a98e4005de11fa506c
cGoyal69 <127645215+cGoyal69@users.noreply.github.com>
2024-06-05 00:27:22 +0530

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,11 +14,11 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/chiraggoyal96/" target="blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/cGoyal69" target="blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/_chirag2004/" target="blank"><img src={navIcon3} alt="" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2042. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
